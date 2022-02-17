aebc8cfcfd210dbc5dc51db6c343328902f3e957
Gustavo Abasto Argote <ifsisgus@gmail.com>
2022-02-17 14:11:37 -0400

    my first commit

---
+++ src/main/java/com/questionsandanswers/questionsandanswers/QuestionsandanswersApplication.java
@@ -0,0 +1,13 @@
+package com.questionsandanswers.questionsandanswers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionsandanswersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionsandanswersApplication.class, args);
+	}
+
+}
